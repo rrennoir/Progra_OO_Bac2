c0ac6b16eaa8d3e98ff702eea1a29b3bfad9834d
rrennoir <32205591+rrennoir@users.noreply.github.com>
2020-10-15 14:25:01 +0200

    Add this. everywhere (:

--- Serie1/src/serie1/Rencontre.java
+++ Serie1/src/serie1/Rencontre.java
@@ -12,41 +12,41 @@ public class Rencontre
 
     public Rencontre(String locaux, String visiteur, int pLocaux, int pVisiteurs, int fLocaux, int fVisiteur)
     {
-        nomLocaux = locaux;
-        nomVisiteurs = visiteur;
-        pointsLocaux = pLocaux;
-        pointsVisiteurs = pVisiteurs;
-        nbFautesLocaux = fLocaux;
-        nbFautesVisiteurs = fVisiteur; 
+        this.nomLocaux = locaux;
+        this.nomVisiteurs = visiteur;
+        this.pointsLocaux = pLocaux;
+        this.pointsVisiteurs = pVisiteurs;
+        this.nbFautesLocaux = fLocaux;
+        this.nbFautesVisiteurs = fVisiteur; 
     }
 
     public Rencontre(String locaux, String visiteur)
     {
-        nomLocaux = locaux;
-        nomVisiteurs = visiteur;
-        pointsLocaux = 0;
-        pointsVisiteurs = 0;
-        nbFautesLocaux = 0;
-        nbFautesVisiteurs = 0; 
+        this.nomLocaux = locaux;
+        this.nomVisiteurs = visiteur;
+        this.pointsLocaux = 0;
+        this.pointsVisiteurs = 0;
+        this.nbFautesLocaux = 0;
+        this.nbFautesVisiteurs = 0; 
     }
 
     public Rencontre(String visiteur)
     {
-        nomLocaux = "Les seigneurs du combat";
-        nomVisiteurs = visiteur;
-        pointsLocaux = 0;
-        pointsVisiteurs = 0;
-        nbFautesLocaux = 0;
-        nbFautesVisiteurs = 0; 
+        this.nomLocaux = "Les seigneurs du combat";
+        this.nomVisiteurs = visiteur;
+        this.pointsLocaux = 0;
+        this.pointsVisiteurs = 0;
+        this.nbFautesLocaux = 0;
+        this.nbFautesVisiteurs = 0; 
     }    
 
     public String vainqueur()
     {
-        if (pointsLocaux > pointsVisiteurs)
-            return nomLocaux;
+        if (this.pointsLocaux > this.pointsVisiteurs)
+            return this.nomLocaux;
 
-        else if (pointsVisiteurs > pointsLocaux)
-            return nomVisiteurs;
+        else if (this.pointsVisiteurs > this.pointsLocaux)
+            return this.nomVisiteurs;
 
         else
             return "Ex aequo";
@@ -54,11 +54,11 @@ public class Rencontre
 
     public String equipeFairPlay()
     {
-        if (nbFautesLocaux < nbFautesVisiteurs)
+        if (this.nbFautesLocaux < this.nbFautesVisiteurs)
             return nomLocaux;
         
-        else if (nbFautesVisiteurs < nbFautesLocaux)
-            return nomVisiteurs;
+        else if (this.nbFautesVisiteurs < this.nbFautesLocaux)
+            return this.nomVisiteurs;
 
         else
             return "Ex aequo";
@@ -66,37 +66,37 @@ public class Rencontre
 
     public boolean exAequo()
     {
-        return pointsLocaux == pointsVisiteurs;
+        return this.pointsLocaux == this.pointsVisiteurs;
     }
 
     public String presenterLocaux()
     {
-        return "L'équipe locale " + nomLocaux;
+        return "L'équipe locale " + this.nomLocaux;
     }
 
     public String presenterVisiteur()
     {
-        return "L'équipe des visiteurs " + nomVisiteurs;
+        return "L'équipe des visiteurs " + this.nomVisiteurs;
     }
 
     public void ajouterPointsLocaux()
     {
-        pointsLocaux++;
+        this.pointsLocaux++;
     }
 
     public void ajouterPointsLocaux(int points)
     {
-        pointsLocaux += points;
+        this.pointsLocaux += points;
     }
 
     public void ajouterPointsVisiteur()
     {
-        pointsVisiteurs++;
+        this.pointsVisiteurs++;
     }
     
     public void ajouterPointsVisiteur(int points)
     {
-        pointsVisiteurs += points;
+        this.pointsVisiteurs += points;
     }
 
     public void ajouterPoints(String equipe)
@@ -110,21 +110,21 @@ public class Rencontre
 
     public void ajouterFauteLocaux()
     {
-        nbFautesLocaux++;
+        this.nbFautesLocaux++;
     }
 
     public void ajouterFauteVisiteur()
     {
-        nbFautesVisiteurs++;
+        this.nbFautesVisiteurs++;
     }
 
     public String presenterAdversaire()
     {
-        return String.format(" %s reçoit %s", presenterLocaux(), presenterVisiteur());
+        return String.format(" %s reçoit %s", this.presenterLocaux(), this.presenterVisiteur());
     }
 
     public String toString()
     {
-        return presenterAdversaire() + "\n" + vainqueur();
+        return this.presenterAdversaire() + "\n" + this.vainqueur();
     }
 }
