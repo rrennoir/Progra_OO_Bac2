e88d0e1b990e0cd7ea6eaa1d1ae53bb36c77f7b9
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2020-12-28 17:54:45 +0100

    Phone V2

--- Examen/src/data/Phone.java
+++ Examen/src/data/Phone.java
@@ -1,8 +1,5 @@
 package data;
 
-import data.exception.DeviceAlreadyExist;
-import data.exception.PhoneNumberAlreadyExist;
-
 import java.util.ArrayList;
 
 public class Phone extends Device
@@ -10,25 +7,38 @@ public class Phone extends Device
     private String number;
     private static ArrayList<String> numbers;
 
-    public Phone(String brand, String model, String serialNumber, int power, String number) throws DeviceAlreadyExist, PhoneNumberAlreadyExist
+    public Phone(String brand, String model, String serialNumber, int power, String number, Room location, Network net) throws DeviceAlreadyExist, PhoneNumberAlreadyExist
     {
-        super(brand, model, serialNumber, power);
-
+        super(brand, model, serialNumber, power , location, net);
+        if (Phone.numbers == null)
+            Phone.numbers = new ArrayList<String>();
+        
         if (!numbers.contains(number))
             this.number = number;
+
         else
             throw new PhoneNumberAlreadyExist(number);
     }
 
     public void turnOn()
     {
-        super.turnOn();
+        super.switchPower(true);
         ring();
         ring();
+    }
+
+    public void turnOff()
+    {
+        super.switchPower(false);
     }
 
     public void ring()
     {
         System.out.println("ring ring");
+    }
+
+    public String toString()
+    {
+        return String.format("%s\nPhone number: %s", super.toString(), this.number);
     }
 }
