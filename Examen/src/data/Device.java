e4a0208a60e41989175fb798af05ce52fc1ce578
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2020-12-28 17:54:11 +0100

    Device V2

--- Examen/src/data/Device.java
+++ Examen/src/data/Device.java
@@ -1,42 +1,66 @@
 package data;
 
 import java.util.ArrayList;
-import data.exception.DeviceAlreadyExist;
 
 public class Device
 {
-    private static ArrayList<String> deviceIDs;
+    private static int idCounter = 0;
+    private static ArrayList<String> deviceIDs = new ArrayList<String>();
+
     private String brand;
     private String model;
     private String serialNumber;
+    private int id;
     private int power;
     private boolean isOn;
-    // network
-    // location
+    private Netint NetInterface;
+    private Room location;
 
-    public Device(String brand, String model, String serialNumber, int power) throws DeviceAlreadyExist
+    public Device(String brand, String model, String serialNumber, int power, Room location, Network net) throws DeviceAlreadyExist
     {
         String deviceID = brand + model + serialNumber;
-
+        
         if (Device.deviceIDs.contains(deviceID))
             throw new DeviceAlreadyExist(brand, model, serialNumber);
         
         else
         {
             Device.deviceIDs.add(deviceID);
+            this.id = getUniqueId();
             this.brand = brand;
             this.model = model;
             this.serialNumber = serialNumber;
             this.isOn = false;
             this.power = power;
+            this.location = location;
+            this.NetInterface = new Netint(net.getNewIp());
         }
     }
 
-    public void turnOn()
+    public int getId()
     {
-        if (true/* connected */)
-        {
-            this.isOn = true;
-        }
+        return this.id;
+    }
+
+    public int powerGive() 
+    {
+    	return this.power;
+    }
+    public void switchPower(boolean value)
+    {
+    	this.isOn = value;
+    }
+
+    public String toString()
+    {
+        return String.format("Id: %d\nBrand: %s\nModel: %s\nSerialNumber: %s", this.id, this.brand, this.model, this.serialNumber);
+    }
+
+    private static int getUniqueId()
+    {
+        int id = Device.idCounter;
+        Device.idCounter += 1;
+
+        return id;
     }
 }
