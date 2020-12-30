ad4e0b4db7ceee620c058f01a10a9850aa94afc3
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2020-12-30 13:12:20 +0100

    stop using space in room and network names

--- Examen/src/data/Data.java
+++ Examen/src/data/Data.java
@@ -17,17 +17,17 @@ public class Data
 	{
 		/** Création des réseaux et placements dans la liste **/
 		this.networks = new HashMap<>();
-		this.networks.put("Network 1", new Network("192.168.1.0", "Network 1"));
-		this.networks.put("Network 2", new Network("192.168.2.0", "Network 2"));
-		this.networks.put("Network 3", new Network("192.168.3.0", "Network 3"));
+		this.networks.put("Network_1", new Network("192.168.1.0","Network_1"));
+		this.networks.put("Network_2", new Network("192.168.2.0","Network_2"));
+		this.networks.put("Network_3", new Network("192.168.3.0","Network_3"));
 
 		/** Création des locaux et placements dans la liste **/
 		this.rooms = new HashMap<>();
-		this.rooms.put("Room 1", new Room("Room 1", 5000));
-		this.rooms.put("Room 2", new Room("Room 2", 6000));
-		this.rooms.put("Room 3", new Room("Room 3", 4500));
-		this.rooms.put("Room 4", new Room("Room 4", 5000));
-		this.rooms.put("Room 5", new Room("Room 5", 4000));
+		this.rooms.put("Room_1", new Room("Room_1", 5000));
+		this.rooms.put("Room_2", new Room("Room_2", 6000));
+		this.rooms.put("Room_3", new Room("Room_3", 4500));
+		this.rooms.put("Room_4", new Room("Room_4", 5000));
+		this.rooms.put("Room_5", new Room("Room_5", 4000));
 
 		this.devices = new HashMap<>();
 	}
